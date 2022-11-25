dcc65ad67b3ef41f97b0b13ef570e9b78aed5968
Roselene Gabun <roselenegabun@gmail.com>
2022-11-25 10:43:57 -0800

    Completed Exercise
    Answered The Following Questions:
    - Who is the Academy Member whose ID is 187?
    
    - Who has have been in at least 3 films?
    
    - Who has a name that starts with "Bob"?
    
    - HARDER: Which Academy Members have been in a film that starts with "A"

--- ES6Scripts/es6_actors.js
+++ ES6Scripts/es6_actors.js
@@ -9,18 +9,68 @@ let academyMembers = [
 function getAcademyMemberByID(_memID){
     let actorName = academyMembers.find(memID).name;
     console.log(actorName)
+    console.log("--------------------")
 
     function memID(value){
         return(value.memID == _memID)
     }
 }
-// Who is the Academy Member whose ID is 187?
-// getAcademyMemberByID(187); //COMMENT OUT
 
-// Who has have been in at least 3 films?
-let films = academyMembers.filter(arrayValue => arrayValue.films.length >= 3);
-films.forEach( arrayValue => console.log(arrayValue.name))
+function getByNumberOfFilms(_films){
+    let actorName = academyMembers.filter(arrayValue => arrayValue.films.length >= _films);
+    actorName.forEach(arrayValue => console.log(arrayValue.name))
+    console.log("--------------------")
+}
 
-// Who has a name that starts with "Bob"?
-// HARDER: Which Academy Members have been in a film
-// that starts with "A"
+function getNameThatStartsWith(_name){
+    let actorName = academyMembers.filter(arrayValue => arrayValue.name.includes(_name));
+    actorName.forEach(arrayValue => console.log(arrayValue.name))
+    console.log("--------------------")
+}
+
+function getActorFromFilmThatStartsWith(_filmStart){
+    let actorName = academyMembers.filter(arrayValue => (arrayValue.films.findIndex(filmStartsAtAFunction) != -1));
+    actorName.forEach(arrayValue => console.log(arrayValue.name))
+    console.log("--------------------")
+
+    function filmStartsAtAFunction(element){
+        return (element.substring(0,1) == _filmStart);
+    } 
+}
+
+/*--------QUESTIONS & ANSWERS-----------*/
+
+// * Who is the Academy Member whose ID is 187?
+getAcademyMemberByID(187); //ANS
+
+// * Who has have been in at least 3 films?
+getByNumberOfFilms(3); //ANS
+
+// * Who has a name that starts with "Bob"?
+getNameThatStartsWith("Bob"); //ANS
+
+// * HARDER: Which Academy Members have been in a film that starts with "A"?
+getActorFromFilmThatStartsWith("A"); //ANS 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ! 💩 function
+// let filteredfilms = academyMembers.filter( (actor) => (actor.films.findIndex(filmStartsAtAFunction) != -1));
+
+// function filmStartsAtAFunction(element){
+//     return (element.substring(0,1) == "A");
+// }
+
+
+// console.log(filteredfilms);
